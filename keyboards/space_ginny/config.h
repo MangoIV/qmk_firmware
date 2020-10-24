7f1b9ec59442c13f501892165745f8fc35218a43
MangoIV <magnus.viernickel@icloud.com>
2020-10-24 14:57:13 +0200

    added space_ginn firmware folder and basic firmware

---
+++ keyboards/space_ginny/config.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID 0x4653
+#define PRODUCT_ID 0x0001
+#define DEVICE_VER 0x0001
+#define MANUFACTURER mangoiv
+#define PRODUCT space_ginny
+#define DESCRIPTION an ASETNIOP device
+
+
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 5
+
+#define MATRIX_ROW_PINS {B5}
+#define MATRIX_COL_PINS {D4, C6, D7, E6, B4}
+#define UNUSED_PINS
+
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+
+
+#define USE_SERIAL
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 30000
+#endif
+
+#define ___ KC_NO
+
+#define LAYOUT( \
+        L01, L02, L03, L04, L05, R01, R02, R03, R04, R05, 
+    ) \
+    { \
+        { L01, L02, L03, L04, L05 }, \
+        { R01, R02, R03, R04, R05 } \
+    }
