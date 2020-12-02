a793f12540c354fbf91589d8d07d52306ce487b2
MangoIV <magnus.viernickel@icloud.com>
2020-12-02 15:15:08 +0100

    adjusted firmware to be able to leave adj, fixed ctrl key

--- keyboards/draculad/keymaps/default/keymap.c
+++ keyboards/draculad/keymaps/default/keymap.c
@@ -16,18 +16,18 @@ extern uint8_t is_master;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] =  LAYOUT( \
     KC_Q,         KC_W, KC_E,    KC_R,    KC_T,                             KC_Y, KC_U, KC_I,    KC_O,   KC_P, \
-    LCTL_T(KC_A), KC_S, KC_D,    KC_F,    KC_G,                             KC_H, KC_J, KC_K ,   KC_L ,  KC_SCLN, \
+    KC_A, KC_S, KC_D,    KC_F,    KC_G,                             KC_H, KC_J, KC_K ,   KC_L ,  KC_SCLN, \
     LSFT_T(KC_Z), KC_X, KC_C,    KC_V,    KC_B,                             KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), \
-                        KC_MUTE, KC_LALT, KC_BSPC, KC_SPC, LT(_NUM,KC_DEL), LT(_SYMB, KC_ENT), KC_CAPS, TG(_ADJ) \
+                        KC_MUTE, KC_LCTL, LALT_T(KC_BSPC), KC_SPC, LT(_NUM,KC_DEL), LT(_SYMB, KC_ENT), KC_CAPS, TG(_ADJ) \
                     ), 
   [_NUM] = LAYOUT( \
     KC_1,         KC_2, KC_3,    KC_4,    KC_5,                             KC_6, KC_7, KC_8,    KC_9,   KC_0, \
-    KC_LCTL, XXXXXXX, KC_VOLD,    KC_VOLU,    XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP ,   KC_RIGHT ,  KC_QUOT, \
+    KC_TAB, XXXXXXX, KC_VOLD,    KC_VOLU,    XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP ,   KC_RIGHT ,  KC_QUOT, \
     KC_LSFT, XXXXXXX, XXXXXXX,    KC_MUTE,    RESET,                        KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_RSFT, \
                         XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, _______, KC_ENT, KC_NO, KC_NO \
     ), 
   [_SYMB] = LAYOUT( \
-    KC_ESC,         KC_F1, KC_F2,    KC_F3,    KC_F4,                             KC_6, KC_7, KC_8,    KC_9,   KC_0, \
+    KC_ESC,         KC_F1, KC_F2,    KC_F3,    KC_F4,                             XXXXXXX, XXXXXXX, XXXXXXX,    KC_EQL,   KC_MINS, \
     XXXXXXX, KC_F5, KC_F6,    KC_F7,    KC_F8,                      KC_LBRC, KC_RBRC, XXXXXXX ,   KC_GRV ,  KC_BSLS, \
     KC_LSFT, KC_F9, KC_F10,    KC_F11,    KC_F12,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT, \
                         XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_NO, KC_NO \
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,         XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,   XXXXXXX, \
     EEP_RST, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,                        RGB_MOD, RGB_HUI, RGB_SAI ,   RGB_VAI ,  XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,                        RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, \
-                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______ \
     ) \
 };
 
@@ -66,8 +66,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     } else {
 	    return OLED_ROTATION_0;
     }
-}
-
+}  
 static void render_logo(void) {
   static const char PROGMEM drac_logo[] = {
     // drac_logo, 128x64px
@@ -141,7 +140,7 @@ static void render_logo(void) {
 
 static void render_status(void) {
   oled_write_P(PSTR("This is\n\nDracul\nLad\n\nv1.0\n\n"), false);
-  sprintf(wpm_as_str, "WPM%03d", get_current_wpm());
+  sprintf(wpm_as_str, "WPM %03d", get_current_wpm());
   oled_write(wpm_as_str,false);
   led_t led_state = host_keyboard_led_state();
   oled_write_P(PSTR("\n\ncaps: "), false);
