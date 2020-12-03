83c8378f04064ee70c4feddc37783c351bf22fea
MangoIV <magnus.viernickel@icloud.com>
2020-12-03 20:15:46 +0100

    adjusted firmware

--- keyboards/draculad/keymaps/default/keymap.c
+++ keyboards/draculad/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,         KC_2, KC_3,    KC_4,    KC_5,                             KC_6, KC_7, KC_8,    KC_9,   KC_0, \
     KC_TAB, XXXXXXX, KC_VOLD,    KC_VOLU,    XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP ,   KC_RIGHT ,  KC_QUOT, \
     KC_LSFT, XXXXXXX, XXXXXXX,    KC_MUTE,    RESET,                        KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_RSFT, \
-                        XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, _______, KC_ENT, KC_NO, KC_NO \
+                        XXXXXXX, XXXXXXX, KC_LALT, XXXXXXX, _______, KC_ENT, KC_NO, KC_NO \
     ), 
   [_SYMB] = LAYOUT( \
     KC_ESC,         KC_F1, KC_F2,    KC_F3,    KC_F4,                             XXXXXXX, XXXXXXX, XXXXXXX,    KC_EQL,   KC_MINS, \
