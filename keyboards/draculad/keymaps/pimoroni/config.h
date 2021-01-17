3c70dce1b551737580f8b70027f049f378dd0f8a
MangoIV <magnus.viernickel@icloud.com>
2021-01-17 20:10:32 +0100

    figured the pimoroni stuff out, works well now

--- keyboards/draculad/keymaps/pimoroni/config.h
+++ keyboards/draculad/keymaps/pimoroni/config.h
@@ -1,1 +1,8 @@
-#define PIMORONI_TRACKBALL_INVERT_X
+//comment that out if your trackball is on the left side
+#define TRACKBALL_RIGHT
+
+#ifdef TRACKBALL_RIGHT
+    #define PIMORONI_TRACKBALL_INVERT_X
+    #define PIMORONI_TRACKBALL_INVERT_Y
+#endif
+
--- keyboards/draculad/keymaps/pimoroni/rules.mk
+++ keyboards/draculad/keymaps/pimoroni/rules.mk
@@ -3,5 +3,3 @@
 POINTING_DEVICE_ENABLE = yes
 SRC += pimoroni_trackball.c
 QUANTUM_LIB_SRC += i2c_master.c
-
-OLED_DRIVER_ENABLE= no
