5e23f1ac22d8d5eb688a22be1d2129b97502f5a5
MangoIV <magnus.viernickel@icloud.com>
2021-01-17 19:56:48 +0100

    tried to get trackball to work again

--- keyboards/draculad/keymaps/pimoroni/keymap.c
+++ keyboards/draculad/keymaps/pimoroni/keymap.c
@@ -25,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,         KC_2, KC_3,    KC_4,    KC_5,                             KC_6, KC_7, KC_8,    KC_9,   KC_0, \
     KC_TAB, XXXXXXX, KC_VOLD,    KC_VOLU,    XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP ,   KC_RIGHT ,  KC_QUOT, \
     KC_LSFT, XXXXXXX, XXXXXXX,    KC_MUTE,    RESET,                        KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_RSFT, \
-                        XXXXXXX, XXXXXXX, KC_LALT, XXXXXXX, _______, KC_ENT, KC_NO, KC_NO \
+                        XXXXXXX, XXXXXXX, KC_LALT, XXXXXXX, KC_NO, _______, KC_ENT, KC_NO \
     ), 
   [_SYMB] = LAYOUT( \
     KC_ESC,         KC_F1, KC_F2,    KC_F3,    KC_F4,                             XXXXXXX, XXXXXXX, XXXXXXX,    KC_EQL,   KC_MINS, \
     XXXXXXX, KC_F5, KC_F6,    KC_F7,    KC_F8,                      KC_LBRC, KC_RBRC, XXXXXXX ,   KC_GRV ,  KC_BSLS, \
     KC_LSFT, KC_F9, KC_F10,    KC_F11,    KC_F12,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT, \
-                        XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_NO, KC_NO \
+                        XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO, _______, KC_NO \
     ),
     [_MUS] = LAYOUT(\
    KC_LCTL, XXXXXXX,    XXXXXXX,  XXXXXXX,XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,   XXXXXXX, \
--- keyboards/draculad/keymaps/pimoroni/rules.mk
+++ keyboards/draculad/keymaps/pimoroni/rules.mk
@@ -1,6 +1,7 @@
-OLED_DRIVER_ENABLE= no
 
 # only uncomment on the side you have your trackball on
 POINTING_DEVICE_ENABLE = yes
 SRC += pimoroni_trackball.c
-QUANTUM_LIB_SRC += i2c_master
+QUANTUM_LIB_SRC += i2c_master.c
+
+OLED_DRIVER_ENABLE= no
