eead2da9e77f63d13130115082d510e29bd884ab
MangoIV <magnus.viernickel@icloud.com>
2021-02-15 23:44:11 +0100

    added a non-combo friendly keymap :)

--- keyboards/le_chiffre/keymaps/mangoiv/keymap.c
+++ keyboards/le_chiffre/keymaps/mangoiv/keymap.c
@@ -17,48 +17,32 @@
 
 enum layers{
   _BASE,
-  _NUM_SYM,
+  _NUM,
   _NAV
 };
 
-enum combo_events {
-  COMBO_BSPC,
-  COMBO_NUMBAK,
-  COMBO_TAB,
-  COMBO_ESC,
-  COMBO_DEL,
-};
 
-#define KC_NUM_SPC LT(_NUM_SYM, KC_SPC)
-#define KC_GA LGUI_T(KC_A)
-#define KC_AS LALT_T(KC_S)
-#define KC_CD LCTL_T(KC_D)
-#define KC_SF LSFT_T(KC_F)
-#define KC_SJ RSFT_T(KC_J)
-#define KC_CK RCTL_T(KC_K)
-#define KC_AL RALT_T(KC_L)
-#define KC_GSCLN RGUI_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  KC_MPLY,  KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
-   KC_GA,  KC_AS,  KC_CD,  KC_SF,   KC_G,            KC_H,  KC_SJ,   KC_CK,  KC_AL, KC_GSCLN,
-    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH,
-                         KC_LCTL, KC_ENT,            KC_NUM_SPC, MO(_NAV)
+    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  KC_MUTE,  KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
+    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,            KC_H,  KC_J,    KC_K,    KC_L,  KC_SCLN,
+    LSFT_T(KC_Z),   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M, KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+                         LALT_T(KC_BSPC),  LCTL_T(KC_SPC),            LT(_NUM,KC_ENT), LT(_NAV,KC_DEL)
   ),
 
-  [_NUM_SYM] = LAYOUT(
-        KC_1,  KC_2,     KC_3,     KC_4,      KC_5,  KC_TRNS,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-    KC_EXLM,  KC_AT,  KC_HASH,   KC_DLR,   KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR, KC_EQUAL,  KC_MINS,
-    KC_BSLS,KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS,            KC_RPRN,  KC_RBRC,  KC_RCBR,   KC_DOT,   KC_GRV,
-                                KC_CAPS,   KC_TRNS,            KC_TRNS,  KC_TRNS
+  [_NUM] = LAYOUT(
+        KC_1,  KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+    KC_TAB,  KC_NO,  KC_VOLD,   KC_VOLU,   KC_NO  ,            KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT,  KC_QUOT,
+    KC_LSFT,KC_NO,  KC_NO,  KC_NO,   KC_NO,            KC_HOME,  KC_END,  KC_PGUP,   KC_PGDN,   KC_RSFT,
+                                KC_LALT,   KC_NO,             KC_TRNS,  RESET
   ),
 
   [_NAV] = LAYOUT(
-      RESET,  _______,  AG_NORM,  AG_SWAP,  DEBUG, KC_TRNS,   KC_GRV,  KC_PGDN,    KC_UP,  KC_PGUP,  KC_SCLN,
-    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_NO,           KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
-    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_NO,           KC_MINS,    KC_RO,  KC_COMM,   KC_DOT,  KC_BSLS,
-                                  KC_TRNS,KC_TRNS,           KC_TRNS,  KC_TRNS
+      KC_ESC, KC_F1,    KC_F2,  KC_F3,  KC_F4, KC_NO,   KC_NO,  KC_NO,    KC_NO,  KC_EQL,  KC_MINS,
+    KC_CAPS,  KC_F5,  KC_F6,  KC_F7,  KC_F8,           KC_LBRC,  KC_RBRC,  KC_NO,  KC_GRV,   KC_BSLS,
+    KC_LSFT,  KC_F9, KC_F10,  KC_F11,  KC_F12,             KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_RSFT,
+                                  KC_LALT,KC_NO,           KC_NO,  KC_TRNS
   ),
 };
 
@@ -71,24 +55,6 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
 }
-
-#ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
-  [COMBO_NUMBAK] = COMBO(combo_numbak,KC_BSPC),
-  [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
-  [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
-  [COMBO_DEL] = COMBO(combo_del,KC_DEL),
-
-};
-#endif
-
 #ifdef OLED_DRIVER_ENABLE  //Special thanks to Sickbabies for this great OLED widget!
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;  // rotates for proper orientation
@@ -115,7 +81,7 @@ static void render_layer_status(void) {
             oled_write_ln_P(PSTR("BASE"), false);
             break;
         case _NUM:
-            oled_write_ln_P(PSTR(" NUM"), false);
+            oled_write_ln_P(PSTR(" SYM"), false);
             break;
         case _NAV:
             oled_write_ln_P(PSTR(" NAV"), false);
